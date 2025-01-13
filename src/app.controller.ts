47cc65fe2c6739c6b8fc9e013d77f08c8dee10ef
Weeraphat2000 <weeraphat.main@gmail.com>
2025-01-13 22:50:21 +0700

    update test action eslint edit2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { sayHello } from './user';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('hello/:name')
+  getHello2(@Param('name') name: string) {
+    return sayHello(name);
   }
 }
